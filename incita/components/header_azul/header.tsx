84225fe6b0f7899661e493476ea334f515c7e7a8
nicolass9118 <fernandes.nicolas.oliveira@gmail.com>
2025-06-26 19:12:58 -0300

    (fix)Header Mobile

--- incita/components/header_azul/header.tsx
+++ incita/components/header_azul/header.tsx
@@ -7,10 +7,22 @@ import { MobileMenu } from "./mobile-menu"
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import { usePathname } from "next/navigation" // Importa o hook
+import { Link } from "lucide-react"
+import { useAuth } from "../../contexts/auth-context"
+import { useRouter } from "next/navigation"
 
 export function HeaderAzul() {
+  const { isLoggedIn, logout } = useAuth()
+  const router = useRouter()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const pathname = usePathname() // Obtém o pathname atual
+
+  const handleLogout = () => {
+    logout()
+    router.push('/login')
+  }
+
+  const isActive = (path: string) => pathname === path
 
   // Não renderiza o header na landing page
   if (pathname === "/landing") {
@@ -42,22 +54,40 @@ export function HeaderAzul() {
               transition={{ duration: 0.3, ease: 'easeInOut' }}
               className="absolute top-23 left-0 right-0 bg-white z-50 p-4 shadow-md md:hidden"
             >
-              <div className="flex flex-col space-y-4">
-                <div className="flex flex-col space-y-2 pt-2">
-                  <a
-                    href="/login"
-                    className="px-6 py-2 rounded-[10px] bg-[transparent] text-base font-medium border-2 border-[#CA9C60] text-[#CA9C60] text-center"
-                  >
-                    Entrar
-                  </a>
-                  <a
-                    href="/register"
-                    className="px-6 py-2 rounded-[10px] text-base font-medium bg-[#CA9C60] text-white text-center"
-                  >
-                    Cadastrar-se
-                  </a>
-                </div>
+              <div className="flex items-center space-x-4">
+                {isLoggedIn ? (
+                  <>
+                    <Link 
+                      href="/perfil"
+                      className={`flex items-center ${isActive("/perfil") ? "text-white" : "text-gray-300 hover:text-white transition-colors"}`}>
+                        Ver Perfil  {/* Aqui vai a foto de perfil */}
+                    </Link>
+                    <button
+                      onClick={handleLogout}
+                      className="px-6 py-3 rounded-[20px] bg-[#CA9C60] text-white text-[20px] hover:bg-[#a68050] duration-200 cursor-pointer"
+                    >
+                      Sair
+                    </button>
+                  </>
+
+                ) : (
+                  <>
+                    <Link
+                      href="/login"
+                      className="px-6 py-3 rounded-[10px] border border-white/30 text-white text-[20px] hover:bg-[#CA9C60] hover:border-[#CA9C60] duration-300 transition-colors"
+                    >
+                      Entrar
+                    </Link>
+                    <Link
+                      href="/register"
+                      className="px-6 py-3 rounded-[10px] bg-[#CA9C60] text-white text-[20px] hover:bg-[#a68050] duration-300 transition-colors"
+                    >
+                      Cadastrar-se
+                    </Link>
+                  </>
+                )}
               </div>
+              
             </motion.div>
           )}
         </AnimatePresence>
