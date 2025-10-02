4d0a3385e2b853b1b86ae68d03fd43fd84e883ee
drey012 <drey01214@gmail.com>
2025-10-02 09:09:37 -0300

    Turmas_professor finalizada, pronta para receber endpoints
    Depois só vou precisar adicionar uma animação de hover nos cards

--- src/components/turmas-professor/CriarTurmaCard.tsx
+++ src/components/turmas-professor/CriarTurmaCard.tsx
@@ -7,7 +7,7 @@ export default function CriarTurmaCard() {
       <div className="relative w-full h-full">
         {/* Ellipse 3 - Fundo com blur */}
         <div 
-          className="absolute w-[760.65px] h-[329.12px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 group-hover:scale-105 group-hover:opacity-70"
+          className="absolute w-[900px] h-[400px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 group-hover:scale-105 group-hover:opacity-70"
           style={{
             background: 'linear-gradient(180deg, #075F70 -17.9%, #075F70 149.65%)',
             opacity: 0.6,
@@ -16,7 +16,7 @@ export default function CriarTurmaCard() {
         />
         
         {/* Card principal */}
-        <div className="absolute w-full h-full max-w-[732px] max-h-[280px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-start p-[27px_27px_27px_30px] gap-5 isolate bg-white shadow-[0px_0px_11.7px_1.8px_rgba(0,0,0,0.25)] rounded-[27px] transition-all duration-300 group-hover:shadow-[0px_0px_20px_3px_rgba(7,95,112,0.3)] group-hover:scale-[1.02]">
+        <Link href="/criar_turma" className="absolute w-full h-full max-w-[90%] max-h-[350px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-start p-[27px_27px_27px_30px] gap-5 isolate bg-white shadow-[0px_0px_11.7px_1.8px_rgba(0,0,0,0.25)] rounded-[27px] transition-all duration-300 group-hover:shadow-[0px_0px_20px_3px_rgba(7,95,112,0.3)] group-hover:scale-[1.02]">
           {/* Botão "Criar turma" - Agora no canto inferior direito */}
           <Link href="/criar_turma" className="flex items-center justify-center p-[6px_20px] absolute w-[188px] h-[42px] right-[30px] bottom-[30px] bg-white rounded-[50px] z-30 transition-all duration-300 hover:bg-[#075F70] hover:shadow-lg hover:scale-105 active:scale-95">
             <span 
@@ -48,7 +48,7 @@ export default function CriarTurmaCard() {
               className="w-full h-full object-contain transition-all duration-500 group-hover:brightness-110"
             />
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
