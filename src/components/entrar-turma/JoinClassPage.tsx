7b8fdb58424ccb4540fbe44a16a6699e55706c55
Drey012 <drey01214@gmail.com>
2025-10-29 14:22:11 -0300

    Ajustes e fidelização da página de entrar turma
    Falta adicionar animações e interatividade

--- src/components/entrar-turma/JoinClassPage.tsx
+++ src/components/entrar-turma/JoinClassPage.tsx
@@ -1,21 +1,19 @@
 import { InfoSection } from "./InfoSection"
-import JoinBox  from "./JoinBox"
+import JoinBox from "./JoinBox"
 
 
 export function EntrarTurmaPage() {
   return (
-    <div className="min-h-screen pt-40 bg-gray-50 flex">
+    <div className="min-h-[calc(100vh-100px)] pt-0 bg-gray-50 flex">
 
-      <div className="flex-1 flex flex-col">
+      <main className="flex-1 relative">
 
-        <main className="flex-1 relative">
+        <div className="relative z-10 p-4 sm:p-8 lg:px-20 lg:py-16 flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-10 lg:gap-12  ">
+          <InfoSection />
+          <JoinBox />
+        </div>
+      </main>
 
-          <div className="relative z-10 p-4 sm:p-8 lg:px-20 lg:py-16 flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-10 lg:gap-12  ">
-            <InfoSection />
-            <JoinBox />
-          </div>
-        </main>
-      </div>
     </div>
   )
 }
