192b08e4ed5fbdcc4638f14b58591b41c9f014e0
Luciano do Nascimento Silva <109688871+LucianoSilva-Dev@users.noreply.github.com>
2026-02-03 22:46:27 -0300

    Update API_BASE_URL to new production endpoint

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://backend-incita-production.up.railway.app'//'http://localhost:3001'
+export const API_BASE_URL = 'https://essayer-server-production.up.railway.app/'//'http://localhost:3001'
 
 // Regex para caracteres permitidos (letras, números e o sinal de '+')
 export const VERIFY_CODE_ALLOWED_CHARS_REGEX = /[^A-Za-z0-9+/]/g
